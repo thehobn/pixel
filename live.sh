bd7648271ef73798662eb2925e012defd1c4b3b7
thehobn <thehobn@gmail.com>
2015-05-12 19:54:20 -0700

    reordered commands again and added status reports

--- live.sh
+++ live.sh
@@ -1,35 +1,75 @@
 #!/bin/bash
 SSD=/dev/disk/by-id/ata-KINGSTON_RBU-SUS151S364GD_50026B7E51D74E12
 KEY=/dev/disk/by-id/usb-Generic_Power_Saving_USB_000000000260-0:0
+CYAN='\033[0;36'
+END='\033[0m'
 
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
+printf "Creating new GPT tables . . ."
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
 parted -s $SSD mktable gpt
 parted -s $KEY mktable gpt
 
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
+printf "Generating keyfile . . ."
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
 dd bs=1024 count=8 if=/dev/urandom of=keyfile iflag=fullblock
+
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
+printf "Encrypting SSD . . ."
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
 cryptsetup luksFormat $SSD keyfile
 cryptsetup open --type luks $SSD sys --key-file=keyfile
 
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
+printf "Creating LVM . . ."
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
 vgcreate sys /dev/mapper/sys
 lvcreate -l +100%FREE sys -n root
 
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
+printf "Making filesystems . . ."
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
 mkfs.btrfs /dev/mapper/sys-root
 btrfs filesystem label /dev/mapper/sys-root sys
 mkfs.btrfs -f $KEY
 btrfs filesystem label $KEY key
 
+
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
+printf "Mounting filesystems . . ."
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
 mount /dev/mapper/sys-root /mnt
 mkdir /mnt/boot
 mount $KEY /mnt/boot
 
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
+printf "Inserting keyfile . . ."
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
+mv keyfile /mnt/boot
 
-cp keyfile /mnt/boot
-pacstrap /mnt base
-echo 'MODULES="btrfs nls_cp437 i915"\nHOOKS="base udev autodetect modconf block encrypt lvm2 filesystems keyboard fsck consolefont"' > /etc/mkinitcpio.conf
-mkinitcpio -p linux
+
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
+printf "Generating fstab . . ."
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
+printf "Running pacstrap . . ."
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
+pacstrap /mnt base
+
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
+printf "Building kernel . . ."
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
+echo 'MODULES="btrfs nls_cp437 i915"\nHOOKS="base udev autodetect modconf block encrypt lvm2 filesystems keyboard fsck consolefont"' > /etc/mkinitcpio.conf
+mkinitcpio -p linux
 
 
+
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
+printf "Starting next stage . . ."
+printf "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${CYAN}**********${END}"
 cp chroot.sh /mnt
 cp user.sh /mnt
 arch-chroot /mnt ./chroot.sh
