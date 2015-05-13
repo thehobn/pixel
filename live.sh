2084b05bbdf4ffb5494d359922b65428c774b450
thehobn <thehobn@gmail.com>
2015-05-12 20:12:50 -0700

    removed BEG and END variables

--- live.sh
+++ live.sh
@@ -1,51 +1,49 @@
 #!/bin/bash
 SSD=/dev/disk/by-id/ata-KINGSTON_RBU-SUS151S364GD_50026B7E51D74E12
 KEY=/dev/disk/by-id/usb-Generic_Power_Saving_USB_000000000260-0:0
-BEG='tput setaf 6 & tput smso'
-END='tput sgr0'
 
-echo "${BEG}Creating new GPT tables . . .${END}"
+echo "$(tput setaf 6 & tput smso)Creating new GPT tables . . .$(tput sgr0)"
 parted -s $SSD mktable gpt
 parted -s $KEY mktable gpt
 
-echo "${BEG}Generating keyfile . . .${END}"
+echo "$(tput setaf 6 & tput smso)Generating keyfile . . .$(tput sgr0)"
 dd bs=1024 count=8 if=/dev/urandom of=keyfile iflag=fullblock
 
-echo "${BEG}Encrypting SSD . . .${END}"
+echo "$(tput setaf 6 & tput smso)Encrypting SSD . . .$(tput sgr0)"
 cryptsetup luksFormat $SSD keyfile
 cryptsetup open --type luks $SSD sys --key-file=keyfile
 
-echo "${BEG}Creating LVM . . .${END}"
+echo "$(tput setaf 6 & tput smso)Creating LVM . . .$(tput sgr0)"
 vgcreate sys /dev/mapper/sys
 lvcreate -l +100%FREE sys -n root
 
-echo "${BEG}Making filesystems . . .${END}"
+echo "$(tput setaf 6 & tput smso)Making filesystems . . .$(tput sgr0)"
 mkfs.btrfs /dev/mapper/sys-root
 btrfs filesystem label /dev/mapper/sys-root sys
 mkfs.btrfs -f $KEY
 btrfs filesystem label $KEY key
 
-echo "${BEG}Mounting filesystems . . .${END}"
+echo "$(tput setaf 6 & tput smso)Mounting filesystems . . .$(tput sgr0)"
 mount /dev/mapper/sys-root /mnt
 mkdir /mnt/boot
 mount $KEY /mnt/boot
 
-echo "${BEG}Inserting keyfile . . .${END}"
+echo "$(tput setaf 6 & tput smso)Inserting keyfile . . .$(tput sgr0)"
 mv keyfile /mnt/boot
 
-echo "${BEG}Generating fstab . . .${END}"
+echo "$(tput setaf 6 & tput smso)Generating fstab . . .$(tput sgr0)"
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
-echo "${BEG}Running pacstrap . . .${END}"
+echo "$(tput setaf 6 & tput smso)Running pacstrap . . .$(tput sgr0)"
 pacstrap /mnt base
 
-echo "${BEG}Building kernel . . .${END}"
+echo "$(tput setaf 6 & tput smso)Building kernel . . .$(tput sgr0)"
 echo 'MODULES="btrfs nls_cp437 i915"\nHOOKS="base udev autodetect modconf block encrypt lvm2 filesystems keyboard fsck consolefont"' > /etc/mkinitcpio.conf
 mkinitcpio -p linux
 
 
 
-echo "${BEG}Starting next stage . . .${END}"
+echo "$(tput setaf 6 & tput smso)Starting next stage . . .$(tput sgr0)"
 mv chroot.sh /mnt
 mv user.sh /mnt
 arch-chroot /mnt ./chroot.sh
